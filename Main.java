7e9e638120e301aad3e93d3735763950965ff7f6
Andreas Rydström <128846041+Monsterardadrys@users.noreply.github.com>
2024-01-12 09:27:18 +0100

    First upload!

---
+++ Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package helloWorld;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		print();
+		
+	}
+	
+	public static void print() {
+	
+	System.out.println("Hello World!!");
+	System.exit(0);
+	}
+
+}
